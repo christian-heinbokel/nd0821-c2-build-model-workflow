1da0bb7d402fd5eeb660dff0c6661972dc88423a
Lennart Heinbokel <lennart@heinbokel.net>
2022-05-29 16:26:34 +0200

    bugfix: actually save data which has been filtered
    - previously the data was saved before the wrong geolocations were filtered
    - now the commands are in the correct order, making the pipeline run

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,12 +34,12 @@ def go(args):
     # Convert last_review to datetime
     df["last_review"] = pd.to_datetime(df["last_review"])
 
-    # Save the clean dataframe to csv
-    df.to_csv(args.output_artifact, index=False)
-
     # remove rows from wrong geolocation
     idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
     df = df[idx].copy()
+
+    # Save the clean dataframe to csv
+    df.to_csv(args.output_artifact, index=False)
 
     # Upload the newly created artifact to W&B
     artifact = wandb.Artifact(
