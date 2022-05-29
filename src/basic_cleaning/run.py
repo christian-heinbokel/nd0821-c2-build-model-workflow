d8efafdcc91cad658f7d28ff0a7f967e38f0ab45
Lennart Heinbokel <lennart@heinbokel.net>
2022-05-29 16:06:34 +0200

    updates cleaning component to remove data from wrong geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,10 @@ def go(args):
     # Save the clean dataframe to csv
     df.to_csv(args.output_artifact, index=False)
 
+    # remove rows from wrong geolocation
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Upload the newly created artifact to W&B
     artifact = wandb.Artifact(
         name=args.output_artifact,
