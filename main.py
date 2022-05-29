181ad5a3519a873b5736d642f8eabdb8f81513d0
Lennart Heinbokel <lennart@heinbokel.net>
2022-05-29 15:59:35 +0200

    adds test_regression_model component to mlflow pipeline

--- main.py
+++ main.py
@@ -110,6 +110,14 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
+            _ = mlflow.run(
+                config["main"]["components_repository"] + "/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest" "",
+                },
+            )
 
             ##################
             # Implement here #
